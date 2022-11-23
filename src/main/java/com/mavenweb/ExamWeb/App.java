7cbfc3c4f5248dc761dcbbded80be28d1db74241
Prithvi <prithvi.metpally@gmail.com>
2022-11-23 12:14:53 +0530

    se lab internal

---
+++ src/main/java/com/mavenweb/ExamWeb/App.java
@@ -0,0 +1,13 @@
+package com.mavenweb.ExamWeb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is SE lab internal exam. The created Maven web project is examweb" );
+    }
+}
